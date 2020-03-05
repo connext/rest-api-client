3faf88971f3e4ea0326bd6c7e41732a8621bf973
Pedro Gomes <pedrogomes94@gmail.com>
2020-03-05 13:57:18 +0100

    add constants

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const EMPTY_CHANNEL_PROVIDER_CONFIG = {
+  freeBalanceAddress: undefined,
+  multisigAddress: undefined,
+  natsClusterId: undefined,
+  natsToken: undefined,
+  nodeUrl: undefined,
+  signerAddress: undefined,
+  userPublicIdentifier: undefined,
+};
