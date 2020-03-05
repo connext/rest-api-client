791d404bc79ae944810eb12d306b6440f8078d7e
Pedro Gomes <pedrogomes94@gmail.com>
2020-03-05 13:57:11 +0100

    add connect endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
+import * as connext from "@connext/client";
+import { IConnextClient } from "@connext/types";
+
 import fastify from "fastify";
 import Helmet from "fastify-helmet";
 import config from "./config";
 import pkg from "../package.json";
+import { EMPTY_CHANNEL_PROVIDER_CONFIG } from "./constants";
 
 const app = fastify({ logger: config.debug });
+
+let client: IConnextClient;
 
 app.register(Helmet);
 
@@ -21,6 +27,26 @@ app.get("/info", (req, res) => {
     description: pkg.description,
     version: pkg.version,
   });
+});
+
+app.post("/connect", async (req, res) => {
+  const { network, ethProviderUrl, nodeUrl, mnemonic } = req.body;
+  try {
+    client = await connext.connect(network, { ethProviderUrl, nodeUrl, mnemonic });
+    const config = { ...EMPTY_CHANNEL_PROVIDER_CONFIG, ...client.channelProvider.config };
+    res.status(200).send({
+      connected: true,
+      freeBalanceAddress: config.freeBalanceAddress,
+      multisigAddress: config.multisigAddress,
+      natsClusterId: config.natsClusterId,
+      natsToken: config.natsToken,
+      nodeUrl: config.nodeUrl,
+      signerAddress: config.signerAddress,
+      userPublicIdentifier: config.userPublicIdentifier,
+    });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
 const [host, port] = config.host.split(":");
