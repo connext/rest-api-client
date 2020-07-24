d7140ee360e68e904fe6d6b4ff16583aa30861e2
Rahul Sethuram <rahul@Rahuls-MacBook-Pro.local>
2020-07-24 15:36:07 +0200

    Make preimage and paymentId optional in linked transfer

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,6 @@
 import { getFileStore } from "@connext/store";
-
 import * as connext from "@connext/client";
+import { getRandomBytes32 } from "@connext/utils";
 import {
   IConnextClient,
   IStoreService,
@@ -213,12 +213,14 @@ export default class Client {
     if (params.assetId === AddressZero) {
       delete params.assetId;
     }
+    const paymentId = params.paymentId || getRandomBytes32();
+    const preImage = params.preImage || getRandomBytes32();
     const response = await client.conditionalTransfer({
       conditionType: ConditionalTransferTypes.LinkedTransfer,
       amount: params.amount,
       recipient: params.recipient,
-      preImage: params.preImage || getRandomBytes32(),
-      paymentId: params.paymentId || getRandomBytes32(),
+      preImage,
+      paymentId,
       assetId: params.assetId,
       meta: params.meta,
     } as PublicParams.ConditionalTransfer);
