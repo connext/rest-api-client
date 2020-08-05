19173e59d98e13e37cf086ca9ecafcd8d1e6a58f
Pedro Gomes <pedrogomes94@gmail.com>
2020-08-05 18:20:31 +0200

    transferOnChain fixes

--- src/helpers/ethereum.ts
+++ src/helpers/ethereum.ts
@@ -1,6 +1,6 @@
 import { IConnextClient } from "@connext/types";
 import { ERC20 } from "@connext/contracts";
-import { Wallet, Contract, providers, constants } from "ethers";
+import { Wallet, Contract, providers, constants, BigNumber } from "ethers";
 
 import { RouteMethods } from "./types";
 
@@ -57,11 +57,11 @@ export async function transferOnChain(params: {
   if (params.assetId === constants.AddressZero) {
     tx = await wallet.sendTransaction({
       to: params.recipient,
-      value: params.amount,
+      value: BigNumber.from(params.amount),
     });
   } else {
-    const token = new Contract(params.assetId, ERC20.abi, params.ethProvider);
-    tx = await token.transfer([params.recipient, params.amount]);
+    const token = new Contract(params.assetId, ERC20.abi, wallet);
+    tx = await token.transfer(params.recipient, BigNumber.from(params.amount));
   }
   if (typeof tx.hash === "undefined") {
     throw new Error("Transaction hash is undefined");
