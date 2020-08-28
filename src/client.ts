fe7d1d6b8edc133cbafa171f7505d95d3caaf602
Rahul Sethuram <rahul@Rahuls-MacBook-Pro.local>
2020-08-28 20:18:36 +0200

    Update package

--- src/client.ts
+++ src/client.ts
@@ -281,7 +281,7 @@ export default class Client {
     params: RouteMethods.PostRequestCollateralRequestParams,
   ): Promise<void> {
     const client = this.getClient();
-    await client.requestCollateral(params.assetId);
+    await client.requestCollateral(params.assetId, params.assetId);
   }
 
   public async swap(
